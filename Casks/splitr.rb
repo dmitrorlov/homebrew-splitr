1d31b71e66ff68fb91f8c3cde0bb497d0b15e909
GitHub Action <action@github.com>
2025-08-28 23:44:56 +0000

    Update Splitr to v0.1.22

--- Casks/splitr.rb
+++ Casks/splitr.rb
@@ -1,25 +1,25 @@
 cask "splitr" do
-  version "0.1.16"
-  sha256 "abc123def456..." # Will be real SHA256 hash
+  version "0.1.22"
+  sha256 "8ecffe41c9553362656af811192a3027e06d95d1ac44e5047f2365ff35a58d4f"
 
-  url "https://github.com/dmitryorlov/splitr/releases/download/v#{version}/Splitr-v#{version}-macos-arm64.zip"
+  url "https://github.com/dmitrorlov/splitr/releases/download/v0.1.22/Splitr-v0.1.22-macos-arm64.zip"
   name "Splitr"
-  desc "MacOS application for advanced split tunneling for L2TP VPN connections"
+  desc "MacOS application for advanced L2TP VPN split tunneling."
   homepage "https://github.com/dmitryorlov/splitr"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
+  # Requires macOS 11.0 or later (Big Sur)
   depends_on macos: ">= :big_sur"
-  depends_on arch: :arm64
 
   app "Splitr.app"
 
-  # No need for postflight xattr removal in official repo
-  # Homebrew handles this automatically
+  # Remove quarantine attributes after installation
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Splitr.app"],
+                   sudo: false
+  end
 
+  # Cleanup
   uninstall quit: "com.dmitryorlov.splitr"
 
   zap trash: [
